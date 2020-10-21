9a60e38be75dfeeaf4c4b4a6d9e5e21a5f523314
Crowdbotics <team@crowdbotics.com>
2020-10-21 12:59:58 +0000

    Update models

--- backend/tuve_21788/wsgi.py
+++ backend/tuve_21788/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tuve_21788.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tuve_21788.settings")
 
 application = get_wsgi_application()
